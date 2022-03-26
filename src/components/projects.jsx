560037555f4599554530672c6e47a60e17eadf68
Veronika <veronika.pilipenko@gmail.com>
2022-03-26 14:06:47 -0400

    first version done!"

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -13,23 +13,24 @@ const AboutMe = () => {
                     <button className="green"></button>
                 </div>
                     <NavBar/>
+                    <div className="heading-top">Projects</div>
                     
-                <div className="info" id="heading1">
-                    ~/InAFlash
-                </div>
+                    <div className="info" id="heading1">
+                        ~/InAFlash
+                    </div>
 
                 <div className="info-inner">
                     <a href="https://inaflash.herokuapp.com/#/" target="_blank"><img src="InAFlash.png" className="project-pictures" /></a>
                     <p>InAFlash, a 500px clone, is an online photo-sharing platform which allows users to create accounts, upload, view, and share photos.  </p>
 
-                    Frontend Frameworks: React, Redux, AJAX, Webpck, Google Maps API <br />
-                    Backend Frameworks: Ruby on Rails, PostgrSQL, AWS S3, BCrypt  <br />
+                    <b><i>Frontend Frameworks:</i></b> React, Redux, AJAX, Webpck, Google Maps API <br />
+                    <b><i>Backend Frameworks:</i></b> Ruby on Rails, PostgrSQL, AWS S3, BCrypt  <br />
 
                     <br/>
 
-                    <a href="https://github.com/VPilipenko334/InAFLASH" target="_blank" id="link"><i class="fa-brands fa-github fa-3x"></i></a>
+                    <a href="https://github.com/VPilipenko334/InAFLASH" target="_blank" id="link"><i class="fa-brands fa-github fa-3x" id="social-link"></i></a>
                     &nbsp; &nbsp;
-                    <a href="https://inaflash.herokuapp.com/#/" target="_blank" id="link"><i class="fa-solid fa-arrow-up-right-from-square fa-3x"></i></a>
+                    <a href="https://inaflash.herokuapp.com/#/" target="_blank" id="link"><i class="fa-solid fa-arrow-up-right-from-square fa-3x" id="social-link"></i></a>
                 </div>
 
                 <br />
@@ -42,13 +43,13 @@ const AboutMe = () => {
                     <a href="https://the-greenmarket.herokuapp.com/" target="_blank"><img src="GreenMarket.png" className="project-pictures" /></a>
                     <p>A MERN stack online directory/ application which allows users to create accounts, browse, purchase and locate local goods and services.  </p>
 
-                    Frontend Frameworks: React, Redux, Axiox, Sass, Webpack<br />
-                    Backend Frameworks: Node.js, MongoDB, Express, Passport.js, AWS S3<br />
+                    <b><i>Frontend Frameworks:</i></b> React, Redux, Axiox, Sass, Webpack<br />
+                    <b><i>Backend Frameworks:</i></b> Node.js, MongoDB, Express, Passport.js, AWS S3<br />
                     <br />
 
-                    <a href="https://github.com/internetmara/GreenMarket/" target="_blank" id="link"><i class="fa-brands fa-github fa-3x"></i></a>
+                    <a href="https://github.com/internetmara/GreenMarket/" target="_blank" id="link"><i class="fa-brands fa-github fa-3x" id="social-link"></i></a>
                     &nbsp; &nbsp;
-                    <a href="https://the-greenmarket.herokuapp.com/" target="_blank" id="link"><i class="fa-solid fa-arrow-up-right-from-square fa-3x"></i></a>
+                    <a href="https://the-greenmarket.herokuapp.com/" target="_blank" id="link"><i class="fa-solid fa-arrow-up-right-from-square fa-3x" id="social-link"></i></a>
                 <br />
 
                 </div>
@@ -66,13 +67,53 @@ const AboutMe = () => {
 
                     <p>The game also has a pop-up instructions module that will greet the player at the beginning of the game, as well as a random coffee recipe generator.</p>
 
-                    Vanilla JavaScript, Webpack, HTML, CSS<br /><br />
+                    <b><i>Made using:</i></b> Vanilla JavaScript, Webpack, HTML, CSS<br /><br />
 
 
-                    <a href="https://github.com/VPilipenko334/coffeehouse-card-catch" target="_blank" id="link"><i class="fa-brands fa-github fa-3x"></i></a>
+                    <a href="https://github.com/VPilipenko334/coffeehouse-card-catch" target="_blank" id="link"><i class="fa-brands fa-github fa-3x" id="social-link"></i></a>
                     &nbsp; &nbsp;
-                    <a href="https://vpilipenko334.github.io/coffeehouse-card-catch/" target="_blank" id="link"><i class="fa-solid fa-arrow-up-right-from-square fa-3x"></i></a>
+                    <a href="https://vpilipenko334.github.io/coffeehouse-card-catch/" target="_blank" id="link"><i class="fa-solid fa-arrow-up-right-from-square fa-3x" id="social-link"></i></a>
                         
+                </div>
+
+                <br/>
+
+                <div className="info" id="heading1">
+                    ~/Portfolio
+                </div>
+
+                <div className="info-inner">
+                    <Link to="/"><img src="portfolio.png" className="project-pictures"/></Link>
+                    <p>
+                        The portfolio website that you are reading this on is completely made by me!
+                        This is a very special project because once I finished bootcamp, I had sketched out the idea
+                        and design for how I wanted to build out my portfolio. I am so happy to say that it's finished now and one of my 
+                        favorite things that I've ever built - visually and how it's coded out!
+                    </p>
+
+                    <b><i>Made using:</i></b> React, Node.js, Webpack, HTML, CSS<br />
+
+                    <p>Oh -- and by the way, I built this entire thing out in less than 24 hours!💖</p>
+                </div>
+
+
+                <div className="heading">
+                    Weblow 
+                </div>
+
+                <div className="info" id="heading2">
+                    ~/RecruitingFromScratch
+                </div>
+
+                <div className="info-inner">
+                    <a href="https://www.recruitingfromscratch.com/" target="_blank"><img src="rfs.png" className="project-pictures" /></a>
+                        <p>
+                            Made with webflow -- an online visual editor platform allows users 
+                            to design, edit and build a website, I came up with the original concept for the design, 
+                            edited with CSS and HTML and incorporated RFS' data using a SEO.
+                        </p>
+
+                        <b><i>Made using:</i></b> Webflow, HTML, CSS<br /><br />
                 </div>
             </div>
         </div>
