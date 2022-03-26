04867d8c3c1302df4c446c28dfbe1c02abb48633
Veronika <veronika.pilipenko@gmail.com>
2022-03-26 12:03:53 -0400

    interactive closing buttons

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './stylesheets/HomePage.css';
 import './stylesheets/Skills.css';
 import './stylesheets/Resume.css';
 import './stylesheets/Contact.css';
+import './stylesheets/NavBar.css';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,11 +6,11 @@ const NavBar = () => {
     return (
         <div className="tab-links">
             <div className="background">
-                <Link to="/about" className="homepage-links">about me &nbsp; < Link to="/"><i className="fa-regular fa-rectangle-xmark" id="closing"></i></Link></Link>
-                <Link to="/projects" className="homepage-links">projects &nbsp;< Link to="/"><i className="fa-regular fa-rectangle-xmark" id="closing"></i></Link></Link>
-                <Link to="/skills" className="homepage-links">skills &nbsp;< Link to="/"><i className="fa-regular fa-rectangle-xmark" id="closing"></i></Link></Link>
-                <Link to="/resume" className="homepage-links">resume &nbsp;< Link to="/"><i className="fa-regular fa-rectangle-xmark" id="closing"></i></Link></Link>
-                <Link to="/contact" className="homepage-links">contact &nbsp;< Link to="/"><i className="fa-regular fa-rectangle-xmark" id="closing"></i></Link></Link>
+                <Link to="/about" className="homepage-links">about me &nbsp; < Link to="/" id="closing"><i className="fa-regular fa-rectangle-xmark" id="closing"></i></Link></Link>
+                <Link to="/projects" className="homepage-links">projects &nbsp;< Link to="/" id="closing"><i className="fa-regular fa-rectangle-xmark" id="closing"></i></Link></Link>
+                <Link to="/skills" className="homepage-links">skills &nbsp;< Link to="/" id="closing"><i className="fa-regular fa-rectangle-xmark" id="closing"></i></Link></Link>
+                <Link to="/resume" className="homepage-links">resume &nbsp;< Link to="/" id="closing"><i className="fa-regular fa-rectangle-xmark" id="closing"></i></Link></Link>
+                <Link to="/contact" className="homepage-links">contact &nbsp;< Link to="/" id="closing"><i className="fa-regular fa-rectangle-xmark" id="closing"></i></Link></Link>
             </div>
         </div>
     )
