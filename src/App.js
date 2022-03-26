0e55754383c9744a7024c08fcda94040bd6fa574
Veronika <veronika.pilipenko@gmail.com>
2022-03-25 20:16:06 -0400

    synced router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import { Routes, Route, Link} from "react-router-dom";
 import './stylesheets/App.css';
+import AboutMe from './components/about_me';
 
 function App() {
   return (
-    <div className="App">
-      hi this works
+    <div className="outer-container">
+      <h1>Hi this is a test</h1>
+        <Routes>
+          <Route path="/about" element={<AboutMe />} />
+        </Routes>
     </div>
   );
 }
---
+++ src/components/about_me.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutMe = () => {
+
+    return (
+        <div>
+            <h1>This is the About Me page</h1>
+        </div>
+    )
+}
+
+export default AboutMe;
