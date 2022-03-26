ceaacab987e1123b6fa01917593a4ac7c7259529
Veronika <veronika.pilipenko@gmail.com>
2022-03-25 20:19:28 -0400

    components and links working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Routes, Route, Link} from "react-router-dom";
 import './stylesheets/App.css';
 import AboutMe from './components/about_me';
+import Projects from './components/projects';
+import Skills from './components/skills';
+import Resume from './components/resume';
+import Contact from './components/contact';
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <h1>Hi this is a test</h1>
         <Routes>
           <Route path="/about" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
     </div>
   );
---
+++ src/components/skills.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Skills = () => {
+
+    return (
+        <div>
+            <h1>This is the Skills page</h1>
+        </div>
+    )
+}
+
+export default Skills;
